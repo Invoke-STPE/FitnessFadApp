9265222874fd97e1cf45a0a9ddc8d1be414e9bdf
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-02-23 09:09:02 +0100

    Layout FitnessFad
    Created a rough image of FitnessFad's layout.

---
+++ FitnessFad.UI/Models/UserSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessFad.UI.Models
+{
+    public class UserSettings
+    {
+        public int Meals { get; set; }
+        public string Goal { get; set; }
+    }
+}
